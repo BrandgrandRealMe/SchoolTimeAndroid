82be8ae570b4dcc310671768c7b79a6ac9b5103e
BrandgrandRealMe <brandon@brandgrand.tech>
2024-09-10 14:38:22 -0500

    Added Info Section to Settings

--- app/src/main/java/com/cyberlabs/schooltime/SettingsFragment.kt
+++ app/src/main/java/com/cyberlabs/schooltime/SettingsFragment.kt
@@ -1,11 +1,41 @@
 package com.cyberlabs.schooltime
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import androidx.preference.Preference
 import com.cyberlabs.schooltime.MainActivity
 import androidx.preference.PreferenceFragmentCompat
+
+
 
 class SettingsFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
+
+        val discordPreference = findPreference("visit_discord") as Preference?
+        discordPreference?.setOnPreferenceClickListener {
+            val discordUrl = "https://discord.gg/Bm6fMsA"
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(discordUrl))
+            startActivity(intent)
+            true
+        }
+
+        val fdroidPreference = findPreference("visit_fdroid") as Preference?
+        fdroidPreference?.setOnPreferenceClickListener {
+            val fdroidUrl = "https://github.com/BrandgrandRealMe/SchoolTimeAndroid"
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(fdroidUrl))
+            startActivity(intent)
+            true
+        }
+
+        val githubPreference = findPreference("visit_github_repo") as Preference?
+        githubPreference?.setOnPreferenceClickListener {
+            val githubUrl = "https://github.com/BrandgrandRealMe/SchoolTimeAndroid"
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(githubUrl))
+            startActivity(intent)
+            true
+        }
+
     }
 }
